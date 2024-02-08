f01bb104a81b4a76ca884ff93241b9b83c98544d
tkjo155 <takajo.sum15@gmail.com>
2024-02-08 11:03:56 +0900

    ec2作成

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,12 +2,22 @@
 import * as cdk from "aws-cdk-lib";
 import * as dotenv from "dotenv";
 import { VPC } from "../lib/vpc";
+import { EC2 } from "../lib/ec2";
 
 const app = new cdk.App();
 const context = app.node.tryGetContext("fishing-sharing-cdk");
 dotenv.config({ path: ".env" });
 
 const vpc = new VPC(app, "cdkFishingVpc", {
+  vpcCidr: context.vpcCidr,
+  env: {
+    region: context.region,
+    account: context.accountId,
+  },
+});
+
+const ec2 = new EC2(app, "cdkFishingEc2", {
+  vpcId: context.vpcId,
   vpcCidr: context.vpcCidr,
   env: {
     region: context.region,
