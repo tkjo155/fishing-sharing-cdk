0e154d00eb1666e5b65ed851b29fde9bfab2d427
tkjo155 <takajo.sum15@gmail.com>
2024-02-08 11:32:44 +0900

    名前修正

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,18 +5,22 @@ import { VPC } from "../lib/vpc";
 import { EC2 } from "../lib/ec2";
 import { RDS } from "../lib/rds";
 
+//CDKアプリケーションの新しいインスタンスを作成
 const app = new cdk.App();
+//"cdk-training" というキーに関連する設定情報を取得
 const context = app.node.tryGetContext("fishing-sharing-cdk");
+//dotenv ライブラリを使用して、.env ファイルから環境変数を読み込む
 dotenv.config({ path: ".env" });
-
+//インポートしたVPCクラスを使用してCDKアプリケーション上に新しいVPCを作成
 const vpc = new VPC(app, "cdkFishingVpc", {
   vpcCidr: context.vpcCidr,
+  // AWS 環境に関する情報
   env: {
     region: context.region,
     account: context.accountId,
   },
 });
-
+//EC2の情報やAWS環境の詳細などの特定のパラメーターを使用してスタックのインスタンスを作成
 const ec2 = new EC2(app, "cdkFishingEc2", {
   vpcId: context.vpcId,
   vpcCidr: context.vpcCidr,
@@ -26,7 +30,7 @@ const ec2 = new EC2(app, "cdkFishingEc2", {
   },
 });
 
-const rds = new RDS(app, "cdkTrainingRds", {
+const rds = new RDS(app, "cdkFishingRds", {
   vpcId: context.vpcId,
   vpcCidr: context.vpcCidr,
   env: {
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,19 +1,19 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
-
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import { Construct } from "constructs";
+//SQS キューと SNS トピックを作成し、その間にサブスクリプションを設定
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // Amazon SQS キューを作成
+    const queue = new sqs.Queue(this, "CdkQueue", {
+      visibilityTimeout: Duration.seconds(300), // メッセージの可視性タイムアウトを300秒に設定
     });
-
-    const topic = new sns.Topic(this, 'CdkTopic');
-
+    // Amazon SNS トピックを作成
+    const topic = new sns.Topic(this, "CdkTopic");
+    // SQS キューを SNS トピックにサブスクライブ
     topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
